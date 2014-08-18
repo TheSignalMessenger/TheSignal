f9f84f571c53c7fc6fa023e8f0a7bdf55704b9b9
larsborn <lars@wallenborn.net>
2014-08-18 22:44:50 +0200

    copied rand methods to Utils

--- src/thesignal/utils/Util.java
+++ src/thesignal/utils/Util.java
@@ -1,6 +1,7 @@
 package thesignal.utils;
 
 import java.math.BigInteger;
+import java.util.Random;
 
 import net.tomp2p.peers.Number160;
 
@@ -16,4 +17,50 @@ public final class Util {
     {
     	return add(val, new Number160(1));
     }
+
+	/**
+	 * Returns a pseudo-random number between 0 and Number160.MAX:VALUE,
+	 * inclusive.
+	 *
+	 * @return random Number160.
+	 * @see java.util.Random#nextBytes()
+	 */
+	public static Number160 randNumber160() {
+
+		// NOTE: Usually this should be a field rather than a method
+		// variable so that it is not re-seeded every call.
+		Random rand = new Random(System.currentTimeMillis());
+
+		// nextInt is normally exclusive of the top value,
+		// so add 1 to make it inclusive
+		byte[] randBytes = new byte[20];
+		rand.nextBytes(randBytes);
+
+		return new Number160(randBytes);
+	}
+
+	/**
+	 * Returns a pseudo-random number between min and max, inclusive. The
+	 * difference between min and max can be at most
+	 * <code>Integer.MAX_VALUE - 1</code>.
+	 *
+	 * @param min
+	 *            Minimum value
+	 * @param max
+	 *            Maximum value. Must be greater than min.
+	 * @return Integer between min and max, inclusive.
+	 * @see java.util.Random#nextInt(int)
+	 */
+	public static int randInt(int min, int max) {
+
+		// NOTE: Usually this should be a field rather than a method
+		// variable so that it is not re-seeded every call.
+		Random rand = new Random(System.currentTimeMillis());
+
+		// nextInt is normally exclusive of the top value,
+		// so add 1 to make it inclusive
+		int randomNum = rand.nextInt((max - min) + 1) + min;
+
+		return randomNum;
+	}
 }
