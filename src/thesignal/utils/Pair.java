0c50a3d796e6c11499a6702b85f0034d56790557
Mehrtürer <Mehrtrer@tsp.no-ip.org>
2014-09-04 23:41:37 +0200

    Basic Pair class.

---
+++ src/thesignal/utils/Pair.java
@@ -0,0 +1,11 @@
+package thesignal.utils;
+
+public class Pair<F, S> {
+    public F first;
+    public S second;
+    
+    public Pair(F first, S second) {
+    	this.first = first;
+    	this.second = second;
+	}
+}
