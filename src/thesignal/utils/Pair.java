2ef91fa1f6741ffcf3cfce0c4d015d5a6d039bd5
Mehrtürer <Mehrtrer@tsp.no-ip.org>
2014-09-06 18:36:36 +0200

    Let the Pair class implement the Comparable interface. The Pair class now only works if the template classes also both implement the Comparable interface.

--- src/thesignal/utils/Pair.java
+++ src/thesignal/utils/Pair.java
@@ -1,11 +1,21 @@
 package thesignal.utils;
 
-public class Pair<F, S> {
+public class Pair<F extends Comparable<F> , S extends Comparable<S>> implements Comparable<Pair<F, S>>{
     public F first;
     public S second;
     
     public Pair(F first, S second) {
     	this.first = first;
     	this.second = second;
+	}
+
+	@Override
+	public int compareTo(Pair<F, S> o) {
+		int compRes = first.compareTo(o.first);
+		if(compRes == 0)
+		{
+			return second.compareTo(o.second);
+		}
+		return compRes;
 	}
 }
