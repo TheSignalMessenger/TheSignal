1ef25c27fa31034325f964615ae9ccc9c2091cec
Lars Wallenborn <lars@wallenborn.net>
2014-09-22 23:38:53 +0200

    removed obsolete code

--- src/thesignal/ExampleSimple.java
+++ src/thesignal/ExampleSimple.java
@@ -2,7 +2,6 @@ package thesignal;
 
 import java.io.IOException;
 import java.io.Serializable;
-import java.lang.reflect.Array;
 import java.net.InetAddress;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -83,52 +82,6 @@ public class ExampleSimple {
 	private HashMap<String, TSPeer> knownPeers;
 	
 	private TreeMultimap<Long, Label> dateTimeLabels = TreeMultimap.create(Ordering.natural(), Ordering.arbitrary());
-
-	/**
-	 * Returns a pseudo-random number between 0 and Number160.MAX:VALUE,
-	 * inclusive.
-	 * 
-	 * @return random Number160.
-	 * @see java.util.Random#nextBytes()
-	 */
-	public static Number160 randNumber160() {
-
-		// NOTE: Usually this should be a field rather than a method
-		// variable so that it is not re-seeded every call.
-		Random rand = new Random(System.currentTimeMillis());
-
-		// nextInt is normally exclusive of the top value,
-		// so add 1 to make it inclusive
-		byte[] randBytes = new byte[20];
-		rand.nextBytes(randBytes);
-
-		return new Number160(randBytes);
-	}
-
-	/**
-	 * Returns a pseudo-random number between min and max, inclusive. The
-	 * difference between min and max can be at most
-	 * <code>Integer.MAX_VALUE - 1</code>.
-	 * 
-	 * @param min
-	 *            Minimum value
-	 * @param max
-	 *            Maximum value. Must be greater than min.
-	 * @return Integer between min and max, inclusive.
-	 * @see java.util.Random#nextInt(int)
-	 */
-	public static int randInt(int min, int max) {
-
-		// NOTE: Usually this should be a field rather than a method
-		// variable so that it is not re-seeded every call.
-		Random rand = new Random(System.currentTimeMillis());
-
-		// nextInt is normally exclusive of the top value,
-		// so add 1 to make it inclusive
-		int randomNum = rand.nextInt((max - min) + 1) + min;
-
-		return randomNum;
-	}
 
 	public ExampleSimple(String name, Number160 peerHash) throws Exception {
 		ownName = name;
