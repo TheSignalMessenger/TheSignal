6cb8caa3560792cdfa97e8ad7e3f72c803944689
Lars Wallenborn <lars@wallenborn.net>
2014-11-17 23:13:27 +0100

    added some events

--- src/thesignal/bus/Bus.java
+++ src/thesignal/bus/Bus.java
@@ -1,10 +1,20 @@
 package thesignal.bus;
 
+import static org.reflections.ReflectionUtils.*;
+
 public class Bus {
+	public Bus() {
+		
+	}
+
 	public void handle(Command command) {
 		// @todo
 	}
 
+	public void raise(Event vent) {
+		// @todo
+	}
+	
 	public void register(EventListener listener) {
 		// @todo
 	}
--- src/thesignal/bus/CommandHandler.java
+++ src/thesignal/bus/CommandHandler.java
@@ -1,5 +1,5 @@
 package thesignal.bus;
 
 public interface CommandHandler {
-	public void handle(Command command);
+	// public void handle(Command command, Bus bus);
 }
