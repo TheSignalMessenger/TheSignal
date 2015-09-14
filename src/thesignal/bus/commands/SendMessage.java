1666b5e807fe5aeef82959184971bab6a6fdb9c7
Mehrtürer <Mehrtrer@tsp.no-ip.org>
2015-09-14 19:38:40 +0200

    SendMessage command implementation.

--- src/thesignal/bus/commands/SendMessage.java
+++ src/thesignal/bus/commands/SendMessage.java
@@ -1,7 +1,12 @@
 package thesignal.bus.commands;
 
 import thesignal.bus.Command;
+import thesignal.entity.TSMessage;
 
 public class SendMessage implements Command {
-
+	public final TSMessage message;
+	
+	public SendMessage(TSMessage message_) {
+		message = message_;
+	}
 }
