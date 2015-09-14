b94d47f0563b85d51586ba1a6c3a3d0692b83110
Mehrtürer <Mehrtrer@tsp.no-ip.org>
2015-09-14 17:58:04 +0200

    Minor TSGroup adjustments (and maybe useless).

--- src/thesignal/entity/TSGroup.java
+++ src/thesignal/entity/TSGroup.java
@@ -1,11 +1,53 @@
 package thesignal.entity;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Collection;
+import java.util.TreeSet;
+
+import javax.naming.OperationNotSupportedException;
 
 public class TSGroup {
-	private ArrayList<TSPeer> members;
-	private HashSet<TSMessage> messages;
 	private String name;
+	private TreeSet<TSPeer> members = new TreeSet<TSPeer>();
+	private TreeSet<TSMessage> messages = new TreeSet<TSMessage>();
+
 	private boolean immutable;
+	
+	public TSGroup(String name_, Collection<TSPeer> members_, Collection<TSMessage> messages_) {
+		name = name_;
+		members.addAll(members_);
+		messages.addAll(messages_);
+		immutable = true;
+	}
+	
+	public void addMessage(TSMessage message) throws OperationNotSupportedException
+	{
+		checkMutability();
+		messages.add(message);
+	}
+	
+	public void addMember(TSPeer peer) throws OperationNotSupportedException
+	{
+		checkMutability();
+		members.add(peer);
+	}
+	
+	public TSGroup(String name_, Collection<TSPeer> members_)
+	{
+		name = name_;
+		members.addAll(members_);
+	}
+	
+	public String name()
+	{
+		return name;
+	}
+	
+	private void checkMutability() throws OperationNotSupportedException
+	{
+		if(immutable)
+		{
+			throw new OperationNotSupportedException("This object is immutable");
+		}
+	}
+	
 }
