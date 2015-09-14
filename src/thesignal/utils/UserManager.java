dd785837c324461cbc2d788ab559c9926ba5d7b2
Mehrtürer <Mehrtrer@tsp.no-ip.org>
2015-09-14 18:01:49 +0200

    UserManager expansion.

--- src/thesignal/utils/UserManager.java
+++ src/thesignal/utils/UserManager.java
@@ -1,12 +1,20 @@
 package thesignal.utils;
 
-import java.util.HashSet;
+import java.util.HashMap;
+
+import net.tomp2p.peers.Number160;
 
 public class UserManager {
-	private HashSet<User> mKnownUsers = new HashSet<User>();
+	private HashMap<Number160, User> mKnownUsers = new HashMap<Number160, User>();
 	private User mSelf;
 	
 	public UserManager(User self) {
 		mSelf = self;
+	}
+	
+	public void addUser(User newUser)
+	{
+		User prevUser = mKnownUsers.put(newUser.hash, newUser);
+		assert(prevUser == null);
 	}
 }
