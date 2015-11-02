2841e491f1c6c9f54681d9dc6677dfcb70b07d74
Lars Wallenborn <lars@wallenborn.net>
2015-11-02 19:47:58 +0100

    implements add method on group manager

--- src/thesignal/manager/GroupManager.java
+++ src/thesignal/manager/GroupManager.java
@@ -2,6 +2,8 @@ package thesignal.manager;
 
 import java.util.List;
 
+import net.tomp2p.peers.Number160;
+
 import com.google.inject.Singleton;
 
 import thesignal.entity.DHTGroup;
@@ -13,8 +15,13 @@ public class GroupManager {
 	public List<DHTGroup> findAll() {
 		return groups;
 	}
-	
+
 	public void add(DHTGroup group) {
 		groups.add(group);
+	}
+
+	public void add(String _name, Number160 _hash) {
+		DHTGroup dhtGroup = new DHTGroup(groups.size(), _name, _hash);
+		add(dhtGroup);
 	}
 }
