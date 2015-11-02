5f6a96b169d0e10356555efb80bbf521677906be
Lars Wallenborn <lars@wallenborn.net>
2015-11-02 20:14:52 +0100

    create GroupMembership Entity

--- src/thesignal/dht/usecase/ReadGroupsFromDHT.java
+++ src/thesignal/dht/usecase/ReadGroupsFromDHT.java
@@ -3,11 +3,19 @@ package thesignal.dht.usecase;
 import thesignal.bus.Bus;
 import thesignal.bus.EventListener;
 import thesignal.bus.events.Connected;
+import thesignal.entity.DHTUser;
+import thesignal.manager.MeManager;
 
-public class ReadGroupsFromDHT implements EventListener<Connected>{
+public class ReadGroupsFromDHT implements EventListener<Connected> {
+	public MeManager meManager;
+
+	public ReadGroupsFromDHT(MeManager _meManager) {
+		meManager = _meManager;
+	}
 
 	@Override
 	public void handle(Connected event, Bus bus) {
-		// TODO Auto-generated method stub
+		DHTUser dhtUser = meManager.getDHTUser();
+		// dhtUser.peer.get(dhtUser.hash).getContentKeys();
 	}
 }
---
+++ src/thesignal/entity/DHTGroupMembership.java
@@ -0,0 +1,7 @@
+package thesignal.entity;
+
+import net.tomp2p.peers.Number160;
+
+public class DHTGroupMembership {
+	Number160 groupLocation;
+}
