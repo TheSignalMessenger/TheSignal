d9c1d7da179631688d2ab7aa94c7390eb1163bfa
Mehrtürer <Mehrtrer@tsp.no-ip.org>
2015-11-12 14:19:24 +0100

    Try to render the messages text in different colors, based on its state.
    Not yet tested.

--- src/thesignal/ui/TSMessageCellRenderer.java
+++ src/thesignal/ui/TSMessageCellRenderer.java
@@ -1,5 +1,6 @@
 package thesignal.ui;
 
+import java.awt.Color;
 import java.awt.Component;
 
 import javax.swing.DefaultListCellRenderer;
@@ -7,8 +8,19 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 
 import thesignal.entity.TSMessage;
+import thesignal.utils.Util;
 
 public class TSMessageCellRenderer extends DefaultListCellRenderer {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1963324608886252023L;
+
+	private static Color ColorSending = Color.ORANGE;
+	private static Color ColorSendOK = Color.CYAN;
+	private static Color ColorSendAck = Color.GREEN;
+	private static Color ColorSendFailed = Color.RED;
+	
 	@Override
 	public Component getListCellRendererComponent(JList list, Object value,
 			int index, boolean isSelected, boolean cellHasFocus) {
@@ -24,6 +36,22 @@ public class TSMessageCellRenderer extends DefaultListCellRenderer {
 		{
 			component.setBackground(list.getBackground());
 			component.setForeground(list.getForeground());
+		}
+		if(message.state.equals(TSMessage.State.Sending))
+		{
+			component.setForeground(Util.mix(component.getForeground(), ColorSending));
+		}
+		else if(message.state.equals(TSMessage.State.SendFailed))
+		{
+			component.setForeground(Util.mix(component.getForeground(), ColorSendFailed));
+		}
+		else if(message.state.equals(TSMessage.State.SendOK))
+		{
+			component.setForeground(Util.mix(component.getForeground(), ColorSendOK));
+		}
+		else if(message.state.equals(TSMessage.State.SendAcknowledged))
+		{
+			component.setForeground(Util.mix(component.getForeground(), ColorSendAck));
 		}
 
 		return component;
