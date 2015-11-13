2b3f798d62507a7f9525324ac227e55776f9a688
Mehrtürer <Mehrtrer@tsp.no-ip.org>
2015-11-13 18:15:45 +0100

    Whenever a SelectionEvent occurs, update the selected group index.

--- src/thesignal/ui/TSGroupUI.java
+++ src/thesignal/ui/TSGroupUI.java
@@ -3,28 +3,42 @@ package thesignal.ui;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import com.google.inject.Inject;
 
 import thesignal.bus.Bus;
 import thesignal.bus.Event;
 import thesignal.bus.EventListener;
-import thesignal.bus.RegisterException;
-import thesignal.bus.events.GotMessages;
-import thesignal.bus.events.GroupAdded;
-import thesignal.bus.events.GroupOrderChanged;
+import thesignal.repository.GroupRepository;
 
 public class TSGroupUI implements EventListener<Event> {
 	private TSGroupsListModel groupsListModel;
 	private JList groupsList;
 	private JScrollPane groupsScrollPane;
+	private GroupRepository groupRepository;
 
-	public TSGroupUI() {
-		groupsListModel = new TSGroupsListModel();
+	@Inject
+	public TSGroupUI(GroupRepository groupRepository_, TSGroupsListModel groupsListModel_) {
+		groupRepository = groupRepository_;
+		
+		groupsListModel = groupsListModel_;
 		groupsList = new TSBaseList(groupsListModel);
 		groupsScrollPane = new JScrollPane(groupsList);
 		groupsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		groupsList.setVisibleRowCount(5);
 
-		groupsList.setCellRenderer(new TSMessageCellRenderer());
+		groupsList.setCellRenderer(new TSGroupCellRenderer());
+		
+		groupsList.addListSelectionListener(new ListSelectionListener() {
+			
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				groupRepository.selectGroup(groupsList.getSelectedIndex());
+			}
+		});
+		
 	}
 
 	public JScrollPane getGroupsScrollPane()
