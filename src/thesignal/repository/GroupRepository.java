efd0fedb9ba2fe71d2ddb6c21c6a325542e402da
Mehrtürer <Mehrtrer@tsp.no-ip.org>
2015-11-13 18:13:15 +0100

    Maybe this should be stored somewhere else, but for now I store the currently selected Group in the Repo as well (this already sounds bad -.-).

--- src/thesignal/repository/GroupRepository.java
+++ src/thesignal/repository/GroupRepository.java
@@ -5,13 +5,13 @@ import java.util.List;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
-import thesignal.entity.DHTGroup;
 import thesignal.entity.TSGroup;
 import thesignal.manager.GroupManager;
 
 @Singleton
 public class GroupRepository {
 	GroupManager groupManager;
+	private int selectedGroupIndex = 0;
 	
 	@Inject
 	public GroupRepository(GroupManager groupManager) {
@@ -20,5 +20,25 @@ public class GroupRepository {
 
 	public List<TSGroup> findAll() {
 		return groupManager.findAll();
+	}
+	
+	public void selectGroup(int index)
+	{
+		selectedGroupIndex = index;
+	}
+	
+	public TSGroup getSelectedGroup()
+	{
+		return groupManager.getGroup(selectedGroupIndex);
+	}
+	
+	public int getNumGroups()
+	{
+		return groupManager.getNumGroups();
+	}
+	
+	public TSGroup getGroup(int index)
+	{
+		return groupManager.getGroup(index);
 	}
 }
