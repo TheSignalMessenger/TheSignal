821731d2353fd7e4769307e1a46de9f4a4391878
Mehrtürer <Mehrtrer@tsp.no-ip.org>
2015-11-23 20:08:09 +0100

    new DHTMessage layout.

--- src/thesignal/entity/DHTMessage.java
+++ src/thesignal/entity/DHTMessage.java
@@ -2,12 +2,9 @@ package thesignal.entity;
 
 import java.io.Serializable;
 
-import net.tomp2p.peers.Number160;
-
 public class DHTMessage implements Serializable{
 	private static final long serialVersionUID = -5115111712520066065L;
 	public long createdDateTime; // epoch milliseconds
 	public String payload;
-	public String senderName;
-	Number160 sender;
+	DHTEntity sender;
 }
