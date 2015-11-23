6b8c548ad1622e97057fbdca4e940a53e0e973d0
Mehrtürer <Mehrtrer@tsp.no-ip.org>
2015-11-23 20:02:46 +0100

    created a UserManager.

---
+++ src/thesignal/manager/UserManager.java
@@ -0,0 +1,20 @@
+package thesignal.manager;
+
+import java.util.HashMap;
+
+import thesignal.entity.DHTEntity;
+import thesignal.entity.User;
+
+public class UserManager {
+	private HashMap<DHTEntity, User> users = new HashMap<DHTEntity, User>();
+	
+	public void addUser(DHTEntity entity, User user)
+	{
+		User prevItem = users.put(entity, user);
+		assert(prevItem == null);
+	}
+
+	public User getUser(DHTEntity entity) {
+		return users.get(entity);
+	}
+}
