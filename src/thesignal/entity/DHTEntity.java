2c9d09998c6a15687f06908767c973464a907c4a
Mehrtürer <Mehrtrer@tsp.no-ip.org>
2015-11-23 19:48:59 +0100

    DHTEntity no longer implements Comparable

--- src/thesignal/entity/DHTEntity.java
+++ src/thesignal/entity/DHTEntity.java
@@ -2,16 +2,10 @@ package thesignal.entity;
 
 import net.tomp2p.peers.Number160;
 
-abstract public class DHTEntity implements Comparable<DHTEntity> {
+abstract public class DHTEntity {
 	public final Number160 hash;
 
 	public DHTEntity(Number160 hash) {
 		this.hash = hash;
-	}
-
-	@Override
-	public int compareTo(DHTEntity o) {
-		int hcomp = hash.compareTo(o.hash);
-		return hcomp;
 	}
 }
