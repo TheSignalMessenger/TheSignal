49c22a5f731db829e69789e36ecaaa8ecbf72550
Mehrtürer <Mehrtrer@tsp.no-ip.org>
2016-01-11 18:42:31 +0100

    Didn't work so well without the MeManager being a singleton :)

--- src/thesignal/manager/MeManager.java
+++ src/thesignal/manager/MeManager.java
@@ -1,8 +1,11 @@
 package thesignal.manager;
 
+import com.google.inject.Singleton;
+
 import net.tomp2p.p2p.Peer;
 import thesignal.entity.User;
 
+@Singleton
 public class MeManager {
 	public User user;
 	public Peer peer;
