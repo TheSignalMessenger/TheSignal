e8daffbc9b07262908dee41ec51eab967275b544
Mehrtürer <Mehrtrer@tsp.no-ip.org>
2016-02-01 17:22:53 +0100

    Initialize the Me from the Prefs.

--- src/thesignal/manager/MeManager.java
+++ src/thesignal/manager/MeManager.java
@@ -1,5 +1,6 @@
 package thesignal.manager;
 
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import net.tomp2p.p2p.Peer;
@@ -7,6 +8,12 @@ import thesignal.entity.User;
 
 @Singleton
 public class MeManager {
-	public User user;
+	public final User user;
 	public Peer peer;
+	
+	@Inject
+	MeManager()
+	{
+		user = Preferences.getMe();
+	}
 }
